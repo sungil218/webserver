3bc189c0fe28d143a538987bbb0a3dc3e5ad2ecb
sungil218 <sungil218@naver.com>
2019-10-10 21:34:41 +0900

    mongoose connected

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require("express");
-const app = express();
+const routes = require("./routes");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+
+const app = express();
 const PORT = 4000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(routes.home,)
+
+
+mongoose.connect("3.15.85.16:27017/Anyang_db", { useMongoClient: true })
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch(e => console.error(e));
+
+  
+function find() {
+    
+}
 
 function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
